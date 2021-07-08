80e990b70929cc9e00fd108f59a6519009ba6434
Berkan KURT <84171373+berkankurt@users.noreply.github.com>
2021-07-08 12:35:38 +0300

    Berkan KURT

--- py_visa_port/port_read.py
+++ py_visa_port/port_read.py
@@ -11,8 +11,10 @@ locale.setlocale(locale.LC_ALL, 'turkish')
 
 cikisvolt=str()
 
+dcload_id='USB0::0x0A69::0x087C::63206EL00431::INSTR'
+ac_id='USB0::0x0A69::0x0883::96160900000094::INSTR'
 
-path="C:\\Users\\berka\\Desktop\\deneme.xlsx"
+path="C:\\Users\\User\\Desktop\\SETUP\\program\\deneme.xlsx"
 wb_obj=openpyxl.load_workbook(path) # belirtilen yerde ki excell aç
 data = wb_obj.active           # aktif et
 
@@ -22,28 +24,25 @@ print("USB Cihazlar:\t",rm.list_resources()) # visa usb port okuma
 usb=list(rm.list_resources())# usb port listesi
 
 zaman=str(time.strftime('%c'))#sistem zman bilgisi okuma
-
-seriNo=str(input("Cihaz seri nunarasını okutunuz:"))
-zaman=zaman[0:9]
+zaman=zaman[0:10]
 data["D10"]=zaman
-data["B10"]=seriNo
 
 
 
 
 #################################################################################    
-if ('ASRL1::INSTR' in usb):#burada ac güç kaynağı bul
+if (ac_id in usb):#burada ac güç kaynağı bul
     print("AC KAYNAK BAĞLANDI")
-    ac = rm.open_resource("ASRL1::INSTR")#belirtilen usb portu açma 
+    ac = rm.open_resource(ac_id)#belirtilen usb portu açma 
 else:
     print("AC KAYNAK BULUNAMADI")
 #################################################################################            
     
-if ('ASRL11::INSTR' in usb):
+if (dcload_id in usb):
     print("DC YÜK BAĞLANDI")#burada dc bul
-    dcload=rm.open_resource("ASRL11::INSTR")
+    dcload=rm.open_resource(dcload_id)
 else:
-    print("BATARYA YÜK BULUNAMADI")
+    print("DC YÜK BULUNAMADI")
         
 #################################################################################  
         
@@ -54,71 +53,75 @@ else:
     print("BATARYA YÜK BULUNAMADI")
 
 #################################################################################
+seriNo=str(input("Cihaz seri nunarasını okutunuz:"))
+data["B10"]=seriNo
+
 #Çıkış gerilim regülasyon testi
 
 try:
+    ac.write("VOLT:AC 220")
+    ac.write("FREQ 50")
     ac.write("OUTP ON")
-    
-    ac.write("VOLT AC 100;FREQ 50")
-    time.sleep(0.5)
+    time.sleep(6)
+    ac.write("VOLT:AC 175")
+    ac.write("FREQ 50")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E3"]=cikisvolt
     
-    ac.write("VOLT AC 220")
-    time.sleep(0.5)
+    ac.write("VOLT:AC 220")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E5"]=cikisvolt
     
-    ac.write("VOLT AC 240")
-    time.sleep(0.5)
+    ac.write("VOLT:AC 240")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E7"]=cikisvolt
     
-    dcload.write("CURR:STAT:L1 39.5")
+    dcload.write("MODE CCH")
+    dcload.write("CURR:STAT:L1 19.5")
     dcload.write("LOAD ON")
-    time.sleep(0.5)
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E8"]=cikisvolt
     
-    ac.write("VOLT AC 220")
-    time.sleep(0.5)
+    ac.write("VOLT:AC 220")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E6"]=cikisvolt
     
-    ac.write("VOLT AC 100")
-    time.sleep(0.5)
+    ac.write("VOLT:AC 175")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["E4"]=cikisvolt
     
 ##############################################################################◘
 #Frekans çıkış ölçüm
 
-    ac.write("VOLT AC 220;FREQ 47")
-    time.sleep(0.5)
+    ac.write("VOLT:AC 220")
+    ac.write("FREQ 47")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["M3"]=cikisvolt
     
     
-    ac.write("VOLT AC 220;FREQ 63")
-    time.sleep(0.5)
+    ac.write("FREQ 63")
+    time.sleep(2)
     cikisvolt=dcload.query("MEAS:VOLT?")
     data["M4"]=cikisvolt
+    ac.write("OUTP OFF")
+    dcload.write("LOAD OFF")
     
-    wb_obj.save(filename="C:\\Users\\berka\\Desktop\\python_ders\\%s.xlsx"%(seriNo))
+    print("Test Başarılı :)")
+    wb_obj.save(filename="C:\\Users\\User\\Desktop\\SETUP\\program\\%s.xlsx"%(seriNo))
     
     
     
 except:
     print("Test hata!")
     data["G10"]="TEST HATASI"
-    wb_obj.save(filename="C:\\Users\\berka\\Desktop\\python_ders\\%s.xlsx"%(seriNo))
+    wb_obj.save(filename="C:\\Users\\User\\Desktop\\SETUP\\program\\%s.xlsx"%(seriNo))
         
-
-    
-
-
-
-
-
 
  
